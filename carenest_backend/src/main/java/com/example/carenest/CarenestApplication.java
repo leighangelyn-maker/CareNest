55056012368177a624ae1dd6ca65280567925195
Angelyn <leighangelyn@gmail.com>
2026-06-09 11:38:19 +0000

    first commit

---
+++ carenest_backend/src/main/java/com/example/carenest/CarenestApplication.java
@@ -0,0 +1,13 @@
+package com.example.carenest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarenestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarenestApplication.class, args);
+	}
+
+}
